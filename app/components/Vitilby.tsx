4fba8ed55fa7c0d6a73e8067c9d092d375f9884e
Tjommilaskus <mps.94@me.com>
2024-06-28 16:47:45 +0200

    Nye bilder endret rekkefølge

--- app/components/Vitilby.tsx
+++ app/components/Vitilby.tsx
@@ -9,21 +9,21 @@ export function Vitilby() {
     <>
       <div className="grid grid-cols-2 gap-4 p-4">
         <a>
-          <Image src={Vei} alt="Fagelig veiledning" width={350} height={350} />
-        </a>
-
-        <a>
           <Image
-            src={TPS}
-            alt="Terapautiske samtaler"
+            src={KursFag}
+            alt="Kurs og Fagdager"
             width={350}
             height={350}
           />
         </a>
+
+        <a>
+          <Image src={Vei} alt="Fagelig veiledning" width={350} height={350} />
+        </a>
         <a>
           <Image
-            src={KursFag}
-            alt="Kurs og Fagdager"
+            src={TPS}
+            alt="Terapautiske samtaler"
             width={350}
             height={350}
           />
