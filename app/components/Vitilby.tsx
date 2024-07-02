82ab0f7e1c8cb6c445b77f699d50fc017fe61d5f
Tjommilaskus <mps.94@me.com>
2024-07-02 16:15:06 +0200

    linker på forside

--- app/components/Vitilby.tsx
+++ app/components/Vitilby.tsx
@@ -3,39 +3,39 @@ import Vei from "../../public/Vei.png";
 import TPS from "../../public/TPS.png";
 import KursFagU from "../../public/KursFagU.png";
 import OA from "../../public/OA.png";
+import Link from "next/link";
 
 export function Vitilby() {
   return (
     <>
-      <div className="grid grid-cols-2 gap-4 p-4">
-        <a>
+      <div className="grid lg:grid-cols-2 gap-4 p-4 grid-cols-1 ">
+        <Link href={"/Tjenester"}>
           <Image
             src={KursFagU}
             alt="Kurs og Fagdager"
-            width={350}
-            height={350}
+            width={450}
+            height={450}
           />
-        </a>
-
-        <a>
-          <Image src={Vei} alt="Fagelig veiledning" width={350} height={350} />
-        </a>
-        <a>
+        </Link>
+        <Link href={"/Tjenester"}>
+          <Image src={Vei} alt="Fagelig veiledning" width={450} height={450} />
+        </Link>
+        <Link href={"/Tjenester"}>
           <Image
             src={TPS}
             alt="Terapautiske samtaler"
-            width={350}
-            height={350}
+            width={450}
+            height={450}
           />
-        </a>
-        <a>
+        </Link>
+        <Link href={"/Tjenester"}>
           <Image
             src={OA}
             alt="Oppfølging og avlastning"
-            width={350}
-            height={350}
+            width={450}
+            height={450}
           />
-        </a>
+        </Link>
       </div>
     </>
   );
