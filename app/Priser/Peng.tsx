b1472e1e4d7555c600b5823bd644a452422a466a
Tjommilaskus <mps.94@me.com>
2024-07-05 17:06:09 +0200

    peng

--- app/Priser/Peng.tsx
+++ app/Priser/Peng.tsx
@@ -1,7 +1,7 @@
 export function Peng() {
   return (
     <div className="relative flex flex-col items-center pt-20">
-      <h1 className="pt-20">Konsultasjon 10.000kr per min!</h1>
+      <h1 className="pt-20">404 kommer snart</h1>
     </div>
   );
 }
