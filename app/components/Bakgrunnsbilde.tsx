77e7e67191cd56b8ef317ab03cd6ef63dbe177e5
Tjommilaskus <mps.94@me.com>
2024-07-17 18:50:32 +0200

    bakgrunn pad

--- app/components/Bakgrunnsbilde.tsx
+++ app/components/Bakgrunnsbilde.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export function Bakgrunnsbilde() {
   return (
-    <div className="relative flex flex-col items-center pt-20">
+    <div className="relative flex flex-col items-center pt-18">
       <Image src={bakgrunn} alt="bakgrunn" />
     </div>
   );
