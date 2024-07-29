8827b273a5c1577a0baaaa7c1ba652504529bbb9
Tjommilaskus <mps.94@me.com>
2024-07-29 15:46:12 +0200

    pls virk

--- app/components/Bakgrunnsbilde.tsx
+++ app/components/Bakgrunnsbilde.tsx
@@ -1,6 +1,14 @@
 import BackgroundVideo from "next-video/background-video";
-import getStarted from "/videos/Infovideo.mp4";
+import getStarted from "/videos/Infovideo-2.mp4";
 
 export function Bakgrunnsbilde() {
-  return <BackgroundVideo placeholder="blur" src={getStarted} loop muted />;
+  return (
+    <BackgroundVideo
+      placeholder="blur"
+      src={getStarted}
+      loop
+      muted
+      thumbnailTime={3}
+    />
+  );
 }
