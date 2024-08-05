8ac3a5f19527b5e4a29c536e155a2d67a07a4ee5
Tjommilaskus <mps.94@me.com>
2024-08-05 08:05:05 +0200

    tilbaketilt til bilde2

---
+++ app/Tjenesteinformasjon3/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Navbar } from "../components/Navbar";
+import { Kontaktinfo } from "../components/Kontaktinfo";
+
+export default function VeiledningsInfo() {
+  return (
+    <>
+      <Navbar />
+
+      <Kontaktinfo />
+    </>
+  );
+}
