f1e8bd927cdb588b249a8e301a6a82b387402b9b
Tjommilaskus <mps.94@me.com>
2024-08-07 18:54:29 +0200

    bilder endret

--- app/Priser/page.tsx
+++ app/Priser/page.tsx
@@ -1,13 +1,14 @@
 import { Navbar } from "../components/Navbar";
 import { Kontaktinfo } from "../components/Kontaktinfo";
 import { Peng } from "./Peng";
+import Taushetsplikt from "../components/taushetsplikt";
 export default function Priser() {
   return (
     <div>
       <Navbar />
       <Peng />
-
       <Kontaktinfo />
+      <Taushetsplikt />
     </div>
   );
 }
